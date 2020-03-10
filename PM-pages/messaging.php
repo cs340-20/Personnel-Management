c085a7a75e084f87319ff133b3508ef6948cab74
cwhita11 <59655123+cwhita11@users.noreply.github.com>
2020-03-09 22:36:32 -0400

    Finish events backend
    added ability to update attendance. desired events/attendance functionality completed

--- PM-pages/messaging.php
+++ PM-pages/messaging.php
@@ -1,84 +1,69 @@
 ﻿<!DOCTYPE html>
 <html>
+
 <head>
-    <link rel="stylesheet" type="text/css" href="../PM-css/styles.css">
-    <title>Messaging</title> 
+  <link rel="stylesheet" type="text/css" href="../PM-css/styles.css">
+  <link rel="stylesheet" type="text/css" href="../PM-css/home-style.css">
+  <link rel="stylesheet" type="text/css" href="../PM-css/messaging-style.css">
+
+  <!-- javascript functions for page -->
+  <script type="text/javascript" src="../PM-script/personnel_script.js"></script>
 
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
- * {box-sizing: border-box;}
+  <title>Personnel Management</title>
 
-input[type=text], select, textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
-}
-
-input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-
-.container {
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-}
-</style>
 </head>
-
-
-
-
 
 <body>
 
-    <!-- Nav-bar links -->
-    <ul>
-        <li><a href="../index.php">Home</a></li>
-        <li><a href="personnel.php">Personnel</a></li>
-        <li><a href="events.php">Events</a></li>
-        <li><a class="active" href="messaging.php">Messaging</a></li>
-    </ul>
+  <!-- HTML for nav links and logo -->
+  <div class="page_container">
+    <div class="nav_bar">
+      <div class="nav_stack nav_logo">
+        <h2>AdminMax</h2>
+      </div>
+      <div class="nav_stack">
+        <div class="nav_links">
+          <div class="n_link">
+            <button class="nav_button nav_button_others" onclick="window.location.href = '../index.php';">Home</button>
+          </div>
+          <div class="n_link">
+            <button class="nav_button nav_button_others" onclick="window.location.href = '../PM-pages/personnel.php';">Personnel</button>
+          </div>
+          <div class="n_link">
+            <button class="nav_button nav_button_others" onclick="window.location.href = '../PM-pages/events.php';">Events</button>
+          </div>
+          <div class="n_link">
+            <button class="nav_button nav_button_others" onclick="window.location.href = '../PM-pages/messaging.php';">Messaging</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <h3>Compose Message</h3>
-    
-    <div class="container">
-        <form action="/action_page.php">
-            <label for="fname">First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <div class="home_body message_bg">
+      <!-- Page content goes here -->
+      <div class="messaging_wrapper">
 
-            <label for="lname">Last Name</label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+        <input type="text" name="msg_to" placeholder="To" class="message_input" style="width: 400px;"/>
+        <input type="text" name="msg_sbj" placeholder="Subject" class="message_input msg_wide" />
 
-   
+        <div id="msg_text" contenteditable="true" class="message_input msg_wide msg_cont">
 
-            <label for="subject">Subject</label>
-            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-            <input type="submit" value="Submit">
-        </form>
+        </div>
+      </div>
     </div>
 
 
-
-
-
+    <div class="footer">
+      <nav>
+        <a class='footer_link' href="../index.php">Home</a> |
+        <a class='footer_link' href="../PM-pages/personnel.php">Personnel</a> |
+        <a class='footer_link' href="../PM-pages/events.php">Events</a> |
+        <a class='footer_link' href="../PM-pages/messaging.php">Messaging</a>
+      </nav>
+      <p class="footer_note">Devs: Alex Whitaker, Cainan Howard, Sammy Awad, Timothy Krenz</p>
+    </div>
 
 </body>
+
 </html>
